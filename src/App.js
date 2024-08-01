07eaf3b133a6b8506c2843aec234971cca58d962
Annamalai V <74824181+Annamalai2310@users.noreply.github.com>
2024-08-01 20:28:09 +0530

    commiting all

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './components/mainpage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
